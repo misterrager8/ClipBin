2eb3e4891d631a9065aa7cca3913406a38745f47
Chemllei Joseph <chemllei0304@gmail.com>
2022-07-22 14:34:16 -0400

    Make navbar sticky, import webview for desktop envs

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from ClipBin import create_app
 import config
+import webview
 
 app = create_app(config)
 
 if __name__ == "__main__":
-    app.run()
+    webview.create_window("ClipBin", app, text_select=True, frameless=True)
+    webview.start()
